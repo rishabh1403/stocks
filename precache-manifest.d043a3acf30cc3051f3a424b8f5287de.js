8496199d53e82acddcadc66058e9e013afd8b9e6
Rishabh Jain <mailrishabh1403@gmail.com>
2020-07-11 19:10:42 +0530

    Updates

---
+++ precache-manifest.d043a3acf30cc3051f3a424b8f5287de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c1e592d48f8aa0d227d63a0965a95a",
+    "url": "/stocks/index.html"
+  },
+  {
+    "revision": "1be7bcee97baaa384950",
+    "url": "/stocks/static/css/main.a25bac32.chunk.css"
+  },
+  {
+    "revision": "3fcdd0ba85e2338e2c9e",
+    "url": "/stocks/static/js/2.936c6b3d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/stocks/static/js/2.936c6b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be7bcee97baaa384950",
+    "url": "/stocks/static/js/main.55be98e9.chunk.js"
+  },
+  {
+    "revision": "e5f1fa152c6fd96dd920",
+    "url": "/stocks/static/js/runtime-main.571b3f36.js"
+  }
+]);
